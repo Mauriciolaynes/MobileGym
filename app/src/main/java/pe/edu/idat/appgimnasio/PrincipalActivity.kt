0c217553c4464caa69a6a9c320ffc2d97f34b040
Mauricio Alonso Laynes Castro <mauriciolaynescastro@gmail.com>
2026-05-31 09:45:42 -0500

    Se agrego las pantallas Login, Menu Principal y Registro

---
+++ app/src/main/java/pe/edu/idat/appgimnasio/PrincipalActivity.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.appgimnasio
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+}
